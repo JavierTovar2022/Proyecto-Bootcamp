71a1b2c59a9d483fdf6a702324504b8f3feb048b
JavierTovar2022 <104512421+JavierTovar2022@users.noreply.github.com>
2022-10-11 18:03:19 -0500

    Add files via upload

---
+++ ListadoJs.js
@@ -0,0 +1,14 @@
+var nombre="Luis Javier Tovar"  
+var edad= 21
+var eres_desarrollador = "true"
+const fecha_cadena = new Date(" january 18 2001")
+const libro={
+    Nombre: "cien ños de soledad",
+    Autor:"Gabriel Garcia Marquez",
+    Fecha: "5 de junio de 1967",
+    url: "http://www.secst.cl/upfiles/documentos/19072016_1207am_578dc39115fe9.pdf"
+
+}
+const lista= new Array (nombre, edad, eres_desarrollador, fecha_cadena, libro );
+
+console.log(lista)
